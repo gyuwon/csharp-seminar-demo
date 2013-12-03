0daa802a6b972e52d678c27cd98a9fb802e1aa43
Gyuwon <gyuwon@live.com>
2013-12-03 19:39:50 +0900

    Nullable GetType

---
+++ Nullable/GetType/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace GetType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int? y = 1;
+            int? z = null;
+
+            Console.WriteLine(typeof(int) == x.GetType());
+            Console.WriteLine(typeof(int?) == y.GetType());
+            Console.WriteLine(y.GetType());
+            Console.WriteLine(z.GetType()); // System.NullReferenceException
+        }
+    }
+}
