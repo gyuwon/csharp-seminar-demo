a6a60f1817841f37aa371713e35ec646c0682242
Gyuwon <gyuwon@live.com>
2013-12-10 23:38:52 +0900

    blocking api

---
+++ AsyncAwait/ContactsManager/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactsManager.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, StringLength(50)]
+        public string FirstName { get; set; }
+        [Required, StringLength(50)]
+        public string LastName { get; set; }
+        [Required, StringLength(50)]
+        public string Email { get; set; }
+    }
+}
